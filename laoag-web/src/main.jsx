0ca63e4ae34cc8015d7a5624559a9bdcf1905359
zjsan <zjsantos25@gmail.com>
2024-12-16 16:45:57 +0800

    Update main.jsx

--- laoag-web/src/main.jsx
+++ laoag-web/src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.css'
 import App from './App.jsx'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
 
 createRoot(document.getElementById('root')).render(
