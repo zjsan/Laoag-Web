9cb75b3cb5925bfad04a9b6ef1750c4cbf9853e2
zjsan <zjsantos25@gmail.com>
2024-12-19 14:51:44 +0800

    Update Hero.jsx

--- laoag-web/src/components/Hero.jsx
+++ laoag-web/src/components/Hero.jsx
@@ -9,7 +9,7 @@ const Hero = () => {
           <div className="hero-contents">
               <img src="/images/laoag-welcome-arc 1.png" alt="An image of the Laoag City Gilbert Bridge"  className="img-fluid rounded" />
               <div className="hero-text">
-                  <p id="hero-first-p" className="lead fs-5 mt-3">Laoag City, known as the <span>Sunshine City</span> is the vibrant capital of Ilocos Norte, located in the northern part of the Philippines. The name <span>Laoag</span> translates to <span>bright</span> or <span>light </span>
+                  <p id="hero-first-p" className="lead fs-5 mt-3 ">Laoag City, known as the <span>Sunshine City</span> is the vibrant capital of Ilocos Norte, located in the northern part of the Philippines. The name <span>Laoag</span> translates to <span>bright</span> or <span>light </span>
                   in the native Ilocano language, symbolizing the warmth and hospitality of its people. The city’s rich history, stunning landscapes, and cultural heritage make it a must-visit destination for travelers seeking to experience the best of the Ilocos region.</p>
                   <p className="lead fs-5 mt-3">Discover the exciting activities and sumptuous dishes of Laoag City in Ilocos Norte. Read this guide to learn about the best time to go, how to go, where to stay, what to see and do, and other travel tips to help you plan your trip to the The Sunshine City of the north.</p>
               </div>
