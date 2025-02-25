fb5e2734be4c7736a5c2b4581b14e4facf0c5fb0
zjsan <zjsantos25@gmail.com>
2025-02-25 11:54:26 +0800

    updating source code

--- laoag-web/src/components/Footer.jsx
+++ laoag-web/src/components/Footer.jsx
@@ -13,53 +13,57 @@ const Footer = () => {
           <p className="footer-intro-credit-p">
             This website is maintained by the Laoag Ilocos Norte Travel
             developers. The content is regularly updated to ensure accuracy.
-            <span>Please contact us for any queries or concerns.</span>
+            <span className="footer-intro-span">
+              Please contact us for any queries or concerns.
+            </span>
           </p>
 
           <Container fluid="lg">
-            <Row>
-              <Col lg>
-                <p>
-                  Laoag Ilocos Norte Travel
-                  <span>
-                    Address: Laoag City, Ilocos Norte, Philippines Phone: +63
-                    [Insert Phone Number] Email: tourism@laoagcity.gov.ph
-                  </span>
-                </p>
-              </Col>
+            <div className="footer-info-section">
+              <Row>
+                <Col lg>
+                  <p>
+                    Laoag Ilocos Norte Travel
+                    <span>
+                      Address: Laoag City, Ilocos Norte, Philippines Phone: +63
+                      [Insert Phone Number] Email: tourism@laoagcity.gov.ph
+                    </span>
+                  </p>
+                </Col>
 
-              <Col lg>
-                <p>
-                  Credits & Acknowledgments
-                  <span>
-                    Photos and Content: Thanks to the Laoag City Tourism Office
-                    and local photographers for their contributions.
-                    <span>Website Developed by: Zainal Santos</span>
-                  </span>
-                </p>
-              </Col>
+                <Col lg>
+                  <p>
+                    Credits & Acknowledgments
+                    <span>
+                      Photos and Content: Thanks to the Laoag City Tourism
+                      Office and local photographers for their contributions.
+                      <span>Website Developed by: Zainal Santos</span>
+                    </span>
+                  </p>
+                </Col>
 
-              <Col lg>
-                <p>
-                  Explore Laoag City
-                  <span>Landmarks & Attractions</span>
-                  <span>Local Cuisine</span>
-                  <span>Festivals & Events</span>
-                  <span>Cultural Heritage</span>
-                  <span>Travel Tips</span>
-                </p>
-              </Col>
+                <Col lg>
+                  <p>
+                    Explore Laoag City
+                    <span>Landmarks & Attractions</span>
+                    <span>Local Cuisine</span>
+                    <span>Festivals & Events</span>
+                    <span>Cultural Heritage</span>
+                    <span>Travel Tips</span>
+                  </p>
+                </Col>
 
-              <Col lg>
-                <p>
-                  Explore Laoag City
-                  <span>
-                    Facebook: [Insert Facebook Link] Instagram: [Insert
-                    Instagram Link] Twitter: [Insert Twitter Link]
-                  </span>
-                </p>
-              </Col>
-            </Row>
+                <Col lg>
+                  <p>
+                    Explore Laoag City
+                    <span>
+                      Facebook: [Insert Facebook Link] Instagram: [Insert
+                      Instagram Link] Twitter: [Insert Twitter Link]
+                    </span>
+                  </p>
+                </Col>
+              </Row>
+            </div>
           </Container>
         </div>
       </section>
