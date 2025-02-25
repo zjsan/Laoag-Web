fe2cc2813426686e7ba0a84abb828e3a85152f2e
zjsan <zjsantos25@gmail.com>
2025-02-25 15:16:40 +0800

    Update Discover.jsx

--- laoag-web/src/components/Discover.jsx
+++ laoag-web/src/components/Discover.jsx
@@ -1,15 +1,26 @@
-
 const Discover = () => {
   return (
-    <section id = "discover-section">
-       <h2>Discover Laoag City</h2>
-        <div className="discover-img img-fluid rounded"> <img src="/images/img8469.jpg" alt="Ilocos Norte Capitol" /></div>
-       
-        <div className="discover-text">
-            <p className="lead fs-6 mt-3 section-paragraphs">Join us as we explore the Sunshine City’s rich history, iconic landmarks, delectable cuisine, and vibrant culture. Laoag is not just a place to visit—it’s a place to experience and remember. Whether you’re planning your first trip or returning to discover more, Laoag City welcomes you with open arms and endless possibilities.</p>
-        </div> 
-    </section>
-  )
-}
+    <section id="discover-section">
+      <div className="discover-contents">
+        <h2>Discover Laoag City</h2>
+        <div className="discover-img img-fluid rounded">
+          {" "}
+          <img src="/images/img8469.jpg" alt="Ilocos Norte Capitol" />
+        </div>
 
-export default Discover
+        <div className="discover-text">
+          <p className="lead fs-6 mt-3 section-paragraphs">
+            Join us as we explore the Sunshine City’s rich history, iconic
+            landmarks, delectable cuisine, and vibrant culture. Laoag is not
+            just a place to visit—it’s a place to experience and remember.
+            Whether you’re planning your first trip or returning to discover
+            more, Laoag City welcomes you with open arms and endless
+            possibilities.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Discover;
