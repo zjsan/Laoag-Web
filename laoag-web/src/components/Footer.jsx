f5756e5378b659c2ed0a360befe7a9552fb6c486
zjsan <zjsantos25@gmail.com>
2025-02-25 09:27:40 +0800

    responsive grid

--- laoag-web/src/components/Footer.jsx
+++ laoag-web/src/components/Footer.jsx
@@ -1,46 +1,58 @@
-
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const Footer = () => {
   return (
     <footer>
-        <section>
-       
+      <section>
         <div className="footer-section">
-        <p>© 2024 Laoag Ilocos Norte Travel. All rights reserved</p>
-        <p>This website is maintained by the Laoag Ilocos Norte Travel developers. The content is regularly updated to ensure accuracy. Please contact us for any queries or concerns.</p>
+          <p>© 2024 Laoag Ilocos Norte Travel. All rights reserved</p>
+          <p>
+            This website is maintained by the Laoag Ilocos Norte Travel
+            developers. The content is regularly updated to ensure accuracy.
+            Please contact us for any queries or concerns.
+          </p>
 
-            <p>
-                Laoag Ilocos Norte Travel
-                <span>
-                    
-                    Address: Laoag City, Ilocos Norte, Philippines
-                    Phone: +63 [Insert Phone Number]
-                    Email: tourism@laoagcity.gov.ph
-                </span>
-            </p>
+          <Container fluid="lg">
+            <Row>
+              <Col lg>
+                <p>
+                  Laoag Ilocos Norte Travel
+                  <span>
+                    Address: Laoag City, Ilocos Norte, Philippines Phone: +63
+                    [Insert Phone Number] Email: tourism@laoagcity.gov.ph
+                  </span>
+                </p>
+              </Col>
 
-            <p>
-                Credits & Acknowledgments
-                <span>
-                    Photos and Content: Thanks to the Laoag City Tourism Office and local photographers for their contributions.
-                Website Developed by: Zainal Santos
-                Privacy Policy | Terms of Service
-                </span>
-            </p>
+              <Col lg>
+                <p>
+                  Credits & Acknowledgments
+                  <span>
+                    Photos and Content: Thanks to the Laoag City Tourism Office
+                    and local photographers for their contributions. Website
+                    Developed by: Zainal Santos Privacy Policy | Terms of
+                    Service
+                  </span>
+                </p>
+              </Col>
 
-            <p>
-                Follow Us
-                <span>
-                    Facebook: [Insert Facebook Link]
-                    Instagram: [Insert Instagram Link]
-                    Twitter: [Insert Twitter Link]
-                </span>
-            </p>
+              <Col lg>
+                <p>
+                  Follow Us
+                  <span>
+                    Facebook: [Insert Facebook Link] Instagram: [Insert
+                    Instagram Link] Twitter: [Insert Twitter Link]
+                  </span>
+                </p>
+              </Col>
+            </Row>
+          </Container>
         </div>
-        </section>
-        
+      </section>
     </footer>
-  )
-}
+  );
+};
 
 export default Footer;
