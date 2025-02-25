320490401abb7c183234c44d74164fa25a9ea83a
zjsan <zjsantos25@gmail.com>
2025-02-25 11:10:00 +0800

    adjusted margins and structure

--- laoag-web/src/components/Footer.jsx
+++ laoag-web/src/components/Footer.jsx
@@ -7,11 +7,13 @@ const Footer = () => {
     <footer>
       <section>
         <div className="footer-section">
-          <p>© 2025 Laoag Ilocos Norte Travel. All rights reserved</p>
-          <p>
+          <p className="footer-intro-credit">
+            © 2025 Laoag Ilocos Norte Travel. All rights reserved
+          </p>
+          <p className="footer-intro-credit-p">
             This website is maintained by the Laoag Ilocos Norte Travel
             developers. The content is regularly updated to ensure accuracy.
-            Please contact us for any queries or concerns.
+            <span>Please contact us for any queries or concerns.</span>
           </p>
 
           <Container fluid="lg">
@@ -31,16 +33,26 @@ const Footer = () => {
                   Credits & Acknowledgments
                   <span>
                     Photos and Content: Thanks to the Laoag City Tourism Office
-                    and local photographers for their contributions. Website
-                    Developed by: Zainal Santos Privacy Policy | Terms of
-                    Service
+                    and local photographers for their contributions.
+                    <span>Website Developed by: Zainal Santos</span>
                   </span>
                 </p>
               </Col>
 
               <Col lg>
                 <p>
-                  Follow Us
+                  Explore Laoag City
+                  <span>Landmarks & Attractions</span>
+                  <span>Local Cuisine</span>
+                  <span>Festivals & Events</span>
+                  <span>Cultural Heritage</span>
+                  <span>Travel Tips</span>
+                </p>
+              </Col>
+
+              <Col lg>
+                <p>
+                  Explore Laoag City
                   <span>
                     Facebook: [Insert Facebook Link] Instagram: [Insert
                     Instagram Link] Twitter: [Insert Twitter Link]
