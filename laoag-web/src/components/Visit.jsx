c0c8c813be5964f3a94572fdeff9cd2942bd9bc5
zjsan <zjsantos25@gmail.com>
2025-02-26 09:47:27 +0800

    adjusting margins

--- laoag-web/src/components/Visit.jsx
+++ laoag-web/src/components/Visit.jsx
@@ -5,15 +5,15 @@ const Visit = () => {
             <h2>Why Visit Laoag?</h2>
             <p className="lead fs-6 mt-3 section-paragraphs">Whether you’re drawn by the allure of its historical landmarks, the beauty of its natural surroundings, or the warmth of its people, Laoag City offers something for every traveler. From exploring ancient churches and colonial structures to enjoying outdoor adventures in the nearby sand dunes and beaches, Laoag is a destination that promises unforgettable experiences.</p>
         </div>
-        <div className="visit-pictures">
+        <div className="visit-pictures-container">
             <div className="visit-pictures-top">
-                <img className="img-fluid rounded" src="/images/7443 (1).jpg" alt="Streetfood vendors in Laoag City"/>
-                <img className="img-fluid rounded" src="/images/8.jpg" alt="Adeventorous 4x4 experience in Laoag Sand Dunes"/>
+                <img className="img-fluid rounded visit-pictures" src="/images/7443 (1).jpg" alt="Streetfood vendors in Laoag City"/>
+                <img className="img-fluid rounded visit-pictures" src="/images/8.jpg" alt="Adeventorous 4x4 experience in Laoag Sand Dunes"/>
             </div>
             
             <div className="visit-pictures-bottom">
-                <img className="img-fluid rounded" src="/images/4672313876_cf3cb4b403_b.jpg" alt="Fort Ilocandia Beach"/>
-                <img className="img-fluid rounded" src="/images/442000598_1673363960156598_4705785343291275072_n.jpg" alt="Daklisan Drummers performing during the opening parade of Pamulinawen Festival"/>
+                <img className="img-fluid rounded visit-pictures" src="/images/4672313876_cf3cb4b403_b.jpg" alt="Fort Ilocandia Beach"/>
+                <img className="img-fluid rounded visit-pictures" src="/images/442000598_1673363960156598_4705785343291275072_n.jpg" alt="Daklisan Drummers performing during the opening parade of Pamulinawen Festival"/>
             </div>
          </div>
     </section>
